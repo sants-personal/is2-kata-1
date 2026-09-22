5d7f560a204a378bd2280f8e395bfb140cf2bf98
santiagogape <santiago.galindo101@alu.ulpgc.es>
2026-09-22 12:17:34 +0100

    person y main (kata 1 v3)
    (cherry picked from commit 0c3e706f3f9ddf648c6358a4b6c715d1f366c111)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,13 @@
-void main() {
+import software.ulpgc.Person;
 
+import static java.lang.IO.println;
+
+void main() {
+    Person person = new Person("persona",LocalDate.now());
+    println(person);
+    person.setName("hi");
+    person.setBirthday(LocalDate.now().minusYears(10).plusDays(1));
+    println(person);
+    person.setBirthday(LocalDate.now().minusYears(10));
+    println(person);
 }
