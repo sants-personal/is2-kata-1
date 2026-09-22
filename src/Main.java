33166615b7b82178ef80a99789c900a1bd1caac8
santiagogape <santiago.galindo101@alu.ulpgc.es>
2026-09-22 11:55:30 +0100

    person and main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+import software.ulpgc.Person;
+
+import static java.lang.IO.println;
+
 void main() {
+    Person person = new Person("santi",LocalDate.of(2004,9,10));
+    println(person);
+    person.setBirthday(LocalDate.now().minusYears(4).plusDays(2));
+    println(person);
+    person.setName("santi2");
+    person.setBirthday(LocalDate.now().minusYears(4));
+    println(person);
 
 }
---
+++ src/software/ulpgc/Person.java
@@ -0,0 +1,48 @@
+package software.ulpgc;
+
+import java.time.LocalDate;
+
+public class Person {
+    private String Name;
+    private LocalDate birthday;
+
+    public Person(String name, LocalDate birthday) {
+        Name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public int age() {
+        LocalDate now = LocalDate.now();
+        int years = now.getYear() - this.birthday.getYear();
+        if (
+                now.getMonthValue() < birthday.getMonthValue() ||
+                (now.getMonthValue() == this.birthday.getMonthValue() && now.getDayOfMonth() < birthday.getDayOfMonth())
+        ) return years - 1;
+        return years;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Name='" + Name + '\'' +
+                ", birthday=" + birthday +
+                ", age=" + age()+
+                '}';
+    }
+}
