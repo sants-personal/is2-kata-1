60c88fe01f0f90db210f60dbeeced82b036e4af2
santiagogape <santiago.galindo101@alu.ulpgc.es>
2026-09-22 12:10:13 +0100

    person y name
    (cherry picked from commit 0c3e706f3f9ddf648c6358a4b6c715d1f366c111)

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
-void main() {
+import software.ulpgc.Person;
 
+import static java.lang.IO.println;
+
+void main() {
+    Person person = new Person("santi",LocalDate.of(2004,9,10));
+    println(person);
+    person.setName("other name");
+    person.setBirthday(LocalDate.now().plusDays(1).minusYears(2));
+    println(person);
 }
